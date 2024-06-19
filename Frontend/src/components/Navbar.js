420e2825ee95d837c013da5858fda92569b5a542
avinash4410 <127472282+avinash4410@users.noreply.github.com>
2024-06-19 18:41:21 +0530

    commit

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,32 +1,34 @@
-// Navbar.js
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
-const padding = {
-    
-    padding: '10px' // Adjust the padding as per your requirement
-};
+
 const AppNavbar = ({ userName, onToggleSidebar }) => {
-    return (
-        <Navbar  expand="md" style={padding}>
-            {/* <Navbar.Brand href="#"></Navbar.Brand> */}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Form className="d-flex">
-                        <FormControl type="search" placeholder="Search" className="mr-2" aria-label="Search" />
-                        <Button variant="outline-success"><FontAwesomeIcon icon={faSearch} /></Button>
-                    </Form>
-                </Nav>
-                <Nav>
-                    <Nav.Link>Welcome, {userName}</Nav.Link>
-                </Nav>
-                <Nav>
-                    <Button variant="outline-dark" onClick={onToggleSidebar}><FontAwesomeIcon icon={faBars} /></Button>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
+  const navbarStyle = {
+    backgroundColor: '#343a', // Background color for the navbar
+    padding: '10px 20px', // Adjust padding as needed
+    borderBottom: '1px solid #dee2e6', // Optional: Add a bottom border
+  };
+
+  return (
+    <Navbar expand="md" style={navbarStyle}>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Form className="d-flex">
+            <FormControl type="search" placeholder="Search" className="mr-2" aria-label="Search" />
+            {/* <Button variant="outline-success"><FontAwesomeIcon icon={faSearch} /></Button> */}
+          </Form>
+        </Nav>
+        <Nav>
+          <Nav.Link style={{ color: '#ffffff' }}>Welcome, {userName}</Nav.Link>
+        </Nav>
+        <Nav>
+          <Button variant="outline-light" onClick={onToggleSidebar}><FontAwesomeIcon icon={faBars} /></Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
+
 export default AppNavbar;
--- Frontend/src/components/Sidebar.js
+++ Frontend/src/components/Sidebar.js
@@ -6,7 +6,9 @@ import { Link } from 'react-router-dom';
 const Sidebar = () => {
   return (
     <Nav className="col-md-2 d-none d-md-block sidebar">
-      <div className="sidebar-sticky"></div>
+      <div className="sidebar-sticky">
+        <h1></h1>
+      </div>
       <Nav.Item>
         <Nav.Link as={Link} to="/create-user">
           Create User
