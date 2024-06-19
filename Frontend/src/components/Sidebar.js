5e6933fe4a6cafedb55f4cb6002697b2ba4a68aa
avinash4410 <127472282+avinash4410@users.noreply.github.com>
2024-06-19 21:03:38 +0530

    commit

--- Frontend/src/components/Sidebar.js
+++ Frontend/src/components/Sidebar.js
@@ -1,21 +1,21 @@
-// Sidebar.js
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
 
 const Sidebar = () => {
   return (
     <Nav className="col-md-2 d-none d-md-block sidebar">
       <div className="sidebar-sticky">
-        <h1></h1>
+        
       </div>
       <Nav.Item>
-        <Nav.Link as={Link} to="/create-user">
+        <Nav.Link as={Link} to="/create-user" className="nav-link link-dark">
           Create User
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} to="/user-list">
+        <Nav.Link as={Link} to="/user-list" className="nav-link link-dark">
           User List
         </Nav.Link>
       </Nav.Item>
